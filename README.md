8c4fd9b85e9abba1694e952ea963a42146525a3a
P302 <P302>
2017-05-01 21:32:38 +0400

    C# intro

---
+++ README.md
@@ -0,0 +1,12 @@
+# P301-01.05.2017
+	
+	C# First App, C# Data Types
+	
+	## Araşdırma
+		1. C#-ın avantajları və dezavantajları haqqında məzun araşdırması
+	
+	
+	## Qaynaqlar
+	
+	1. https://www.tutorialspoint.com/csharp/csharp_data_types.htm
+		
---
+++ WpfApp1/P301.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myelements
+{
+    class P301
+    {
+        public int sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static implicit operator int(P301 v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Kamran
+    {
+        public int bolme(int a,int b)
+        {
+            return a/ b;
+        }
+    }
+}
